675536e800cc93a4a69a3834c89fc3e148dabffc
carol <carol.z.hanna@gmail.com>
2025-05-19 20:20:14 +0100

    Reconstructing bug

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Delete.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/Delete.java
@@ -21,7 +21,6 @@ package org.apache.hadoop.fs.shell;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.List;
 
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
@@ -29,7 +28,6 @@ import org.apache.hadoop.fs.PathIOException;
 import org.apache.hadoop.fs.PathIsDirectoryException;
 import org.apache.hadoop.fs.PathIsNotDirectoryException;
 import org.apache.hadoop.fs.PathIsNotEmptyDirectoryException;
-import org.apache.hadoop.fs.PathNotFoundException;
 import org.apache.hadoop.fs.Trash;
 
 /**
@@ -71,19 +69,6 @@ class Delete {
       ignoreFNF = cf.getOpt("f");
       deleteDirs = cf.getOpt("r") || cf.getOpt("R");
       skipTrash = cf.getOpt("skipTrash");
-    }
-
-    @Override
-    protected List<PathData> expandArgument(String arg) throws IOException {
-      try {
-        return super.expandArgument(arg);
-      } catch (PathNotFoundException e) {
-        if (!ignoreFNF) {
-          throw e;
-        }
-        // prevent -f on a non-existent glob from failing
-        return new LinkedList<PathData>();
-      }
     }
 
     @Override
