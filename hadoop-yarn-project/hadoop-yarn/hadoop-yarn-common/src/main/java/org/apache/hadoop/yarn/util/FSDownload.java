85dcbbb8a8623f729dbae3025658ac75a1c306a3
carol <carol.z.hanna@gmail.com>
2025-05-19 23:43:43 +0100

    Reconstructing bug

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/util/FSDownload.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-common/src/main/java/org/apache/hadoop/yarn/util/FSDownload.java
@@ -82,13 +82,6 @@ public class FSDownload implements Callable<Path> {
     return resource;
   }
 
-  private void createDir(Path path, FsPermission perm) throws IOException {
-    files.mkdir(path, perm, false);
-    if (!perm.equals(files.getUMask().applyUMask(perm))) {
-      files.setPermission(path, perm);
-    }
-  }
-
   private Path copy(Path sCopy, Path dstdir) throws IOException {
     FileSystem sourceFs = sCopy.getFileSystem(conf);
     Path dCopy = new Path(dstdir, sCopy.getName() + ".tmp");
@@ -151,9 +144,9 @@ public class FSDownload implements Callable<Path> {
     } while (files.util().exists(tmp));
     destDirPath = tmp;
 
-    createDir(destDirPath, cachePerms);
+    files.mkdir(destDirPath, cachePerms, false);
     final Path dst_work = new Path(destDirPath + "_tmp");
-    createDir(dst_work, cachePerms);
+    files.mkdir(dst_work, cachePerms, false);
 
     Path dFinal = files.makeQualified(new Path(dst_work, sCopy.getName()));
     try {
